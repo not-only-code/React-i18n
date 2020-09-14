0c30df1f4bb86d6ad84f2e7c8bda7af43fa39345
Carlos Sanz Garcia <studiocam6@yahoo.es>
2020-09-14 11:12:18 +0200

    Fixed exportation at "index.js" file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export { default as Reacti18nContext } from './Reacti18nContext';
-export { default as Reacti18nProvider } from './Reacti18nProvider';
+import Reacti18nContext from './Reacti18nContext';
+import Reacti18nProvider from './Reacti18nProvider';
+
+export { Reacti18nContext, Reacti18nProvider };
